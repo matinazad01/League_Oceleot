7cd3fe1aa13b0631e06ef210b67b31fdc2bcf2c3
Munchman <league@Admins-Mac-mini.local>
2017-03-21 18:29:35 -0700

    Drew spirals with robots used for (int)

--- src/TriangleShell.java
+++ src/TriangleShell.java
@@ -4,30 +4,35 @@ public class TriangleShell {
 
 	// 1. Create a new Robot
 
-	
+	Robot matin=new Robot ();
 	void go() {
-		drawTriangle(100); // 3. delete this line (used only for testing)
+		 // 3. delete this line (used only for testing)
 
 		// 6. Make the robot go as fast as possible
 
 		// 4. make a variable to hold the length of the triangle and set it to 50
-
-		// 7. Do the following (up to step 10) 60 times
+       int x = 50;
+       
+       // 7. Do the following (up to step 10) 60 times
 
 			// 9. Change the color of the pen to a random color
 	
 			// 8. Increase the length of the side by 10 pixels
 	
 			// 5. call your drawTriangle() method using your length variable
-	
+    drawTriangle(x);
 			// 10. Turn the tortoise 6 degrees to the right
 
 	}
 
 	/* 2. fill in the method below to draw a triangle. Use the length variable for the size of the triangle. */
 	private void drawTriangle(int length) {
-		
-		
+		for (int i = 0; i < 3; i++) {
+		matin.setSpeed(10);
+			matin.penDown();
+		matin.turn(360/3);
+		matin.move(200);
+		}
 		
 	}
 
